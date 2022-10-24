681f395846898c901622d47baf206a45530bb649
Jeff Price <jlprice888@gmail.com>
2022-10-24 12:24:57 -0400

    Capstone 1 97.5% complete

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -28,15 +28,18 @@ public class VendingMachineCLI {
 	public VendingMachineCLI() {
 	}
 
+
+
 	public void run() {
-	//	int menuNumber = 0;
+		//	int menuNumber = 0;
 		boolean menu = true;
 		BigDecimal balance = new BigDecimal("0.00");
-		System.out.println("Welcome to the Vendo-Matic 800!");
+		System.out.println("Welcome to the Vendo-Matic 800!\n");
 		this.getInventory();
 		File logFile = new File(logPath);
-
-		displayPrint display = new displayPrint();
+		Scanner userInput = new Scanner(System.in);
+		DisplayPrint display = new DisplayPrint();
+		String input = "";
 
 
 		while (menu) {
@@ -47,27 +50,28 @@ public class VendingMachineCLI {
 
 			display.displayMainMenu();
 
-			Scanner userInput = new Scanner(System.in);
-			String input = userInput.nextLine();
+			input = display.getInputFromUser("Please make your selection ");
 
 
-			if (input != null) {
-				switch (input) {
-					case "1": {
-						//this.getInventory();
+			switch (input) {
+				case "1": {
+					//this.getInventory();
 //						for(Item x : itemList) {
 //							if(x.getAmountRemaining() > 0) 	{
-//								System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" + x.getName() + ": " + x.getAmountRemaining() + " More Left");
+//								System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" + x.getName()
+//								+ ": " + x.getAmountRemaining() + " More Left");
 //							} else {
-//								System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" + x.getName() + ": " + "SOLD OUT");
+//								System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" + x.getName() +
+//								": " + "SOLD OUT");
 //							}
 //						}
-						display.displayInventory( itemList);
-						break;
-					} case "2": //The Purchase Menu
-					{
-						boolean loopChecker = true;
-						while (loopChecker) {
+					display.displayInventory(itemList);
+					break;
+				}
+				case "2": //The Purchase Menu
+				{
+					boolean loopChecker = true;
+					while (loopChecker) {
 
 //							System.out.println("Current Money Provided: $" + balance);
 //							System.out.println("");
@@ -76,160 +80,162 @@ public class VendingMachineCLI {
 //							System.out.println("(3) Finish Transaction");
 //							System.out.println("Enter 'X' to return to the main menu.");
 
-							display.displayPurchaseMenu(balance);
+						display.displayPurchaseMenu(balance);
 
-							String purchaseMenuInput = userInput.nextLine().toLowerCase();
-							//Purchase Menu input begins here
-							switch (purchaseMenuInput)
+						String purchaseMenuInput = display.getInputFromUser("Please make your selection ");
+						//Purchase Menu input begins here
+						switch (purchaseMenuInput) {
+							case "1": // Purchase Menu (Feed Money)
 							{
-								case "1": // Purchase Menu (Feed Money)
-								{
-									while (true) {
-										System.out.println("Please enter the amount you would like to add in whole dollars only.");
-										System.out.println("Enter 'X' to go back.");
-										String moneyFeederInput = userInput.nextLine();
-										if(moneyFeederInput.equalsIgnoreCase("X")){break;}
+								while (true) {
+									//   System.out.println("Please enter the amount you would like to add in whole dollars only.");
+									//    System.out.println("Enter 'X' to go back.");
+									String moneyFeederInput = display.getInputFromUser("Please enter the amount you would like to +" +
+											" add in whole dollars only\n Enter X to go back ");
+									if (moneyFeederInput.equalsIgnoreCase("X")) {
+										break;
+									}
+									try (final FileOutputStream fos = new FileOutputStream(logFile, true);
+										 final PrintWriter writer = new PrintWriter(fos)) {
+										int moneyAdded = Integer.parseInt(moneyFeederInput);
+										if (moneyAdded < 0) {
+											throw new RuntimeException();
+										}
+										BigDecimal newMoney = new BigDecimal(moneyAdded);
+										balance = balance.add(newMoney);
+										System.out.println("Your current balance is: $" + balance);
+										System.out.println("");
+										LocalDate nowDate = LocalDate.now();
+										LocalTime nowTime = LocalTime.now();
+										writer.printf("%s %s FEED MONEY: $%s.00 $%s\n",
+												nowDate.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")),
+												nowTime.format(DateTimeFormatter.ofPattern("KK:mm:ss a")), newMoney, balance);
+									} catch (FileNotFoundException fnfe) {
+										System.out.println("could not create the file");
+									} catch (Exception e) {
+										System.out.println("Please enter a positive, WHOLE dollar amount.");
+										System.out.println("");
+									}
+
+								}
+								loopChecker = true;
+								break;
+							}
+							case "2": // Purchase Menu (Select Product)
+							{
+								//this.getInventory();
+//									for(Item x : itemList) {
+//										if(x.getAmountRemaining() > 0) {
+//											System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" +
+//											x.getName() + ": " + x.getAmountRemaining() + " More Left");
+//										} else {
+//											System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" +
+//											x.getName() + ": " + "SOLD OUT");
+//										}
+//									}
+								display.displayInventory(itemList);
+								//   System.out.println("");
+								//   System.out.println("Enter slot number of the item you would like ");
+								String itemSelect = display.getInputFromUser("Enter slot number of the item you would like ");
+								for (Item x : itemList) {
+
+									if (x.getSlot().equalsIgnoreCase(itemSelect) && x.getAmountRemaining() <= 0) {
+										System.out.println("Sorry, this item is Sold Out.");
+										System.out.println("");
+									} else if (x.getSlot().equalsIgnoreCase(itemSelect) && balance.compareTo(x.getCost()) < 0) {
+										System.out.println("You do not have enough money to purchase this product.");
+										System.out.println("");
+									} else if (x.getSlot().equalsIgnoreCase(itemSelect) && balance.compareTo(x.getCost()) >= 0) {
+										balance = balance.subtract(x.getCost());
+										x.amountRemaining -= 1;
 										try (final FileOutputStream fos = new FileOutputStream(logFile, true);
 											 final PrintWriter writer = new PrintWriter(fos)) {
-											int moneyAdded = Integer.parseInt(moneyFeederInput);
-											if (moneyAdded < 0) {
-												throw new RuntimeException();
-											}
-											BigDecimal newMoney = new BigDecimal(moneyAdded);
-											balance = balance.add(newMoney);
-											System.out.println("Your current balance is: $" + balance);
-											System.out.println("");
 											LocalDate nowDate = LocalDate.now();
 											LocalTime nowTime = LocalTime.now();
-											writer.printf("%s %s FEED MONEY: $%s.00 $%s\n", nowDate.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")), nowTime.format(DateTimeFormatter.ofPattern("KK:mm:ss a")),newMoney, balance);
+											writer.printf("%s %s %s %s $%s $%s\n", nowDate.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")),
+													nowTime.format(DateTimeFormatter.ofPattern("KK:mm:ss a")), x.getName(), x.getSlot(),
+													x.getCost(), balance);
 										} catch (FileNotFoundException fnfe) {
 											System.out.println("could not create the file");
 										} catch (Exception e) {
-											System.out.println("Please enter a positive, WHOLE dollar amount.");
-											System.out.println("");
+											System.out.println("An error occurred");
 										}
-
-									}
-									loopChecker = true;
-									break;
-								}
-								case "2": // Purchase Menu (Select Product)
-								{
-									//this.getInventory();
-//									for(Item x : itemList) {
-//										if(x.getAmountRemaining() > 0) {
-//											System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" + x.getName() + ": " + x.getAmountRemaining() + " More Left");
-//										} else {
-//											System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" + x.getName() + ": " + "SOLD OUT");
-//										}
-//									}
-									display.displayInventory( itemList);
-									System.out.println("");
-									System.out.println("Enter slot number of the item you would like ");
-									String itemSelect = userInput.nextLine();
-									for (Item x : itemList) {
-
-										if(x.getSlot().equalsIgnoreCase(itemSelect) && x.getAmountRemaining() <= 0) {
-											System.out.println("Sorry, this item is Sold Out.");
-											System.out.println("");
-										}
-										else if(x.getSlot().equalsIgnoreCase(itemSelect) && balance.compareTo(x.getCost()) < 0) {
-											System.out.println("You do not have enough money to purchase this product.");
-											System.out.println("");
-										}
-										else if (x.getSlot().equalsIgnoreCase(itemSelect) && balance.compareTo(x.getCost()) >= 0) {
-											balance = balance.subtract(x.getCost());
-											x.amountRemaining -= 1;
-											try (final FileOutputStream fos = new FileOutputStream(logFile, true);
-												 final PrintWriter writer = new PrintWriter(fos)) {
-												LocalDate nowDate = LocalDate.now();
-												LocalTime nowTime = LocalTime.now();
-												writer.printf("%s %s %s %s $%s $%s\n", nowDate.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")), nowTime.format(DateTimeFormatter.ofPattern("KK:mm:ss a")),x.getName(),x.getSlot(), x.getCost(), balance);
-											} 	catch (FileNotFoundException fnfe) {
-												System.out.println("could not create the file");
-											}	catch (Exception e) {
-												System.out.println("An error occurred");
-											}
 
 
 										//add x.getCost to the sales report revenue
-											//open log files to log the transaction
-											System.out.println("Item " + x.getName() + " Cost " + x.getCost() + " Money Remaining " + balance);
-											System.out.println(x.getSound());
-											System.out.println("");
-
-										}
+										//open log files to log the transaction
+										System.out.println("Item " + x.getName() + " Cost " + x.getCost() + " Money Remaining " + balance);
+										System.out.println(x.getSound());
+										System.out.println("");
 
 									}
 
-									loopChecker = true;
-									break;
 								}
-								case "3": // Purchase Menu (Finish Transaction)
-								{
-									BigDecimal amountToBeReturned = new BigDecimal("100.00");
-									amountToBeReturned = amountToBeReturned.multiply(balance);
-									BigDecimal oldBalance = new BigDecimal("0.00");
-									oldBalance = oldBalance.add(balance);
-									int balanceAsInteger = amountToBeReturned.intValueExact();
-									int quarters = 0;
-									int dimes = 0;
-									int nickels = 0;
-									quarters = balanceAsInteger / 25;
-									balanceAsInteger -= 25*quarters;
-									if (balanceAsInteger != 0)
-									{
-										dimes = balanceAsInteger / 10;
-										balanceAsInteger -= 10*dimes;
-									}
-									if( balanceAsInteger != 0)
-									{
-										nickels = balanceAsInteger / 5;
-										balanceAsInteger -= 5*nickels;
-									}
-									System.out.println("Your balance is: $" + balance);
-									System.out.println("Here is your change.");
-									System.out.println(quarters + " Quarters, " + dimes + " Dimes, and " + nickels + " Nickels was returned from the vending machine.");
-									balance = balance.subtract(balance);
-									try (final FileOutputStream fos = new FileOutputStream(logFile, true);
-										 final PrintWriter writer = new PrintWriter(fos)) {
-										LocalDate nowDate = LocalDate.now();
-										LocalTime nowTime = LocalTime.now();
-										writer.printf("%s %s GIVE CHANGE: $%s $%s\n", nowDate.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")), nowTime.format(DateTimeFormatter.ofPattern("KK:mm:ss a")),oldBalance,balance);
-									} 	catch (FileNotFoundException fnfe) {
-										System.out.println("could not create the file");
-									}	catch (Exception e) {
-										System.out.println("An error occurred");
-									}
-									System.out.println("Thank you for your patronage!");
-									System.out.println("");
 
-									loopChecker = false;
-									break;
+								loopChecker = true;
+								break;
+							}
+							case "3": // Purchase Menu (Finish Transaction)
+							{
+								BigDecimal amountToBeReturned = new BigDecimal("100.00");
+								amountToBeReturned = amountToBeReturned.multiply(balance);
+								BigDecimal oldBalance = new BigDecimal("0.00");
+								oldBalance = oldBalance.add(balance);
+								int balanceAsInteger = amountToBeReturned.intValueExact();
+								int quarters = 0;
+								int dimes = 0;
+								int nickels = 0;
+								quarters = balanceAsInteger / 25;
+								balanceAsInteger -= 25 * quarters;
+								if (balanceAsInteger != 0) {
+									dimes = balanceAsInteger / 10;
+									balanceAsInteger -= 10 * dimes;
 								}
+								if (balanceAsInteger != 0) {
+									nickels = balanceAsInteger / 5;
+									balanceAsInteger -= 5 * nickels;
+								}
+								System.out.println("Your balance is: $" + balance);
+								System.out.println("Here is your change.");
+								System.out.println(quarters + " Quarters, " + dimes + " Dimes, and " + nickels +
+										" Nickels was returned from the vending machine.");
+								balance = balance.subtract(balance);
+								try (final FileOutputStream fos = new FileOutputStream(logFile, true);
+									 final PrintWriter writer = new PrintWriter(fos)) {
+									LocalDate nowDate = LocalDate.now();
+									LocalTime nowTime = LocalTime.now();
+									writer.printf("%s %s GIVE CHANGE: $%s $%s\n", nowDate.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")),
+											nowTime.format(DateTimeFormatter.ofPattern("KK:mm:ss a")), oldBalance, balance);
+								} catch (FileNotFoundException fnfe) {
+									System.out.println("could not create the file");
+								} catch (Exception e) {
+									System.out.println("An error occurred");
+								}
+								System.out.println("Thank you for your patronage!");
+								System.out.println("");
+
+								loopChecker = false;
+								break;
+							}
 //								case "x": //Purchase menu return to Main Menu
 //								{
 //									loopChecker = false;
 //									break;
 //								}
 
-							}
-						} break;
+						}
 					}
-					case "3":
-					{
-						System.out.println("Thank you for using the Vendo-Matic 800! Umbrella Corp will see you again soon...");
-						menu = false;
-						break;
-					}
-
-					default:
-					{
-						System.out.println("I'm sorry, I did not understand that.");
-					}
+					break;
 				}
-			} else {
-				System.out.println("I'm sorry, I did not understand that.");
+				case "3": {
+					System.out.println("Thank you for using the Vendo-Matic 800! Umbrella Corp will see you again soon...");
+					menu = false;
+					break;
+				}
+
+				default: {
+					System.out.println("I'm sorry, I did not understand that.");
+				}
 			}
 		}
 	}
@@ -238,11 +244,11 @@ public class VendingMachineCLI {
 	public List<Item> getInventory() {
 		//create a file object for the datafile
 		File inventoryFile = new File(filePath);
-		Scanner fileReader;
+		//   Scanner fileReader;
 
 		//opens the file
 		try {
-			fileReader = new Scanner(inventoryFile);
+			Scanner fileReader = new Scanner(inventoryFile);
 			//creates a read loop until the end of the file contents have been reached
 			while (fileReader.hasNextLine()) {
 				String dataFileOutput = fileReader.nextLine();
@@ -257,7 +263,7 @@ public class VendingMachineCLI {
 				BigDecimal itemCost = new BigDecimal(outputFormatted[2]);
 				//Fourth index (3) is the Item Type, which will determine how it is constructed.
 				String itemType = outputFormatted[3];
-				switch(outputFormatted[3]) {
+				switch (outputFormatted[3]) {
 					case "Gum":
 						GumItem gum = new GumItem(slot, name, itemCost, itemType);
 						itemList.add(gum);
@@ -275,12 +281,13 @@ public class VendingMachineCLI {
 						itemList.add(drink);
 						break;
 				}
-			}} catch (FileNotFoundException e) {
+			}
+		} catch (FileNotFoundException e) {
 			System.out.println("File not found.");
 		}
-			return itemList;
-
+		return itemList;
 	}
+
 
 }
 
--- capstone/src/main/java/com/techelevator/displayPrint.java
+++ capstone/src/main/java/com/techelevator/displayPrint.java
@@ -1,15 +1,20 @@
 package com.techelevator;
 
-import java.math.BigDecimal;
-import java.util.List;
 
-public class displayPrint {
+import java.math.BigDecimal;
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DisplayPrint {
 
     public void displayInventory(List<Item> itemList) {
 
         for(Item x : itemList) {
             if(x.getAmountRemaining() > 0) {
-                System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" + x.getName() + ": " + x.getAmountRemaining() + " More Left");
+                System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" + x.getName() + ": "
+                        + x.getAmountRemaining() + " More Left");
             } else {
                 System.out.println(x.getSlot() + "|" + x.getItemType() + "|" + x.getCost() + "|" + x.getName() + ": " + "SOLD OUT");
             }
@@ -22,16 +27,29 @@ public class displayPrint {
         System.out.println("(1) Feed Money");
         System.out.println("(2) Select Product");
         System.out.println("(3) Finish Transaction and Return to Main Menu");
+        System.out.println(" ");
 //        System.out.println("Enter 'X' to return to the main menu.");
     }
 
     public void displayMainMenu () {
-        System.out.println("Please make your selection.");
+        //  System.out.println("Please make your selection.");
         System.out.println("(1) Display Vending Machine Items");
         System.out.println("(2) Purchase");
         System.out.println("(3) Exit");
+        System.out.println(" ");
     }
 
+    public String getInputFromUser (String prompt) {
+        Scanner input = new Scanner(System.in);
+        String userInput = " ";
+        System.out.print(prompt);
+        userInput = input.nextLine().toLowerCase();
+        return userInput;
+    }
+
+
 }
+
+
 
 
